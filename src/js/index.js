94e4f6d0ebacc9637c2abff119713a200abffd4a
gin <gin-18@qq.com>
2024-02-28 14:50:01 +0800

    chore: enable html for markdown it

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,8 @@ const markdownit = require('markdown-it')
 
 // 渲染首页的content
 const content = $('#content')
-const md = markdownit()
+const md = markdownit({
+  html: true
+})
 const result = md.render(content.data().content)
 content.html(result)
-
